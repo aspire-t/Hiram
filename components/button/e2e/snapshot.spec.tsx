4eee1f82d7bc17746b4b1b12fd93799fa79b8eb3
aspire-t <zhijianwubu@gmail.com>
2021-10-07 23:24:07 +0800

    feat:jest

---
+++ components/button/e2e/snapshot.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot'
+import Button from '..'
+import 'jest-environment-puppeteer'
+
+const toMatchSnapshot = configureToMatchImageSnapshot({
+	customSnapshotsDir: `${process.cwd()}/snapshots`,
+	customDiffDir: `${process.cwd()}/diffSnapshots`,
+})
+expect.extend({ toMatchSnapshot })
+
+describe('Button snapshot', () => {
+	it('screenshot should correct', async () => {
+		await jestPuppeteer.resetPage()
+		await page.goto(`file://${process.cwd()}/tests/index.html`)
+		const html = ReactDOMServer.renderToString(<Button>按钮</Button>)
+		await page.evaluate((innerHTML: string) => {
+			document.querySelector('#root')!.innerHTML = innerHTML
+		}, html)
+		const screenshot = await page.screenshot()
+		expect(screenshot).toMatchSnapshot()
+	})
+})
